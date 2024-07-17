9ec120d839561184e4248e54d7df7cc8d6acea45
unknown <nischup>
2024-07-18 00:08:01 +0600

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'accel-smart-iot-app',
+  webDir: 'www'
+};
+
+export default config;
