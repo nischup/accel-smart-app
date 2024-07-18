7ab99422997f069993870518394e5718861d5cdc
nischup <ahsabbir23@gmail.com>
2024-07-18 17:49:09 +0600

    progress bar added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  temperature: number = 25;
+  temperaturePercent: number = 0; // Initialize the property
 
-  constructor() {}
+  constructor() { 
+    this.updateTemperaturePercent();
+  }
 
+  ngOnInit() {}
+
+  updateTemperaturePercent() {
+    // Assuming the temperature range is between 15 and 30 degrees
+    this.temperaturePercent = ((this.temperature - 15) / 15) * 100;
+  }
 }
